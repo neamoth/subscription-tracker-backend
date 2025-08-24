668d391294f1b97502bfda683ed11bcc3955e90d
neamoth <neamoth15-8202@diu.edu.bd>
2025-08-24 10:27:43 +0600

    connect mongodb database

---
+++ app.js
@@ -0,0 +1,25 @@
+import express from "express";
+
+import { PORT } from './config/env.js'
+
+import userRouter  from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import connectToDatabase from "./database/mongodb.js";
+
+
+const app = express();
+
+app.use('/api/v1/users', userRouter)
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/subscriptions', subscriptionRouter)
+
+app.get("/", (req, res) => {
+  res.send("Welcome to our 1st API Subkab");
+});
+
+app.listen(PORT, async() => {
+  console.log(`Subkab is running at PORT http://localhost:${PORT}`);
+
+  await connectToDatabase(); 
+});
