0902983a5f16076a912be47255f2ff0323d971ff
neamoth <neamoth15-8202@diu.edu.bd>
2025-08-24 10:26:57 +0600

    add user, auth & subscription routes

---
+++ routes/subscription.routes.js
@@ -0,0 +1,24 @@
+import { Router } from 'express'
+
+const subscriptionRouter = Router();
+
+subscriptionRouter.get('/', (req,res) => res.send({ title : "GET all subscription"}))
+
+subscriptionRouter.get('/:id', (req,res) => res.send({ title : "GET subscription"}))
+
+subscriptionRouter.post('/', (req,res) => res.send({ title : "CREATE subscription"}))
+
+subscriptionRouter.put('/:id', (req,res) => res.send({ title : "UPDATE subscription"}))
+
+subscriptionRouter.delete('/:id', (req,res) => res.send({ title : "DELETE subscription"}))
+
+//upto this we have done our CURD operations, with get we've read,
+//with post we've create, with put we've update, with delete we've deleled the subscription
+
+subscriptionRouter.get('/user/:id', (req,res) => res.send({ title : "GET all user subscription"}))
+
+subscriptionRouter.put('/:id/cancel', (req,res) => res.send({ title : "CANCEL subscription"}))
+
+subscriptionRouter.get('/upcoming-renewals', (req,res) => res.send({ title : "GET upcoming renewals"}))
+
+export default subscriptionRouter;
