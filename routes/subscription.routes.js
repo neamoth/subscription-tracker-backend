da7a0617325d665159e58e17a12d3aa61c028394
neamoth <neamoth15-8202@diu.edu.bd>
2025-08-25 15:24:55 +0600

    add subscriptions controller to get or create subscriptions

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,4 +1,6 @@
 import { Router } from 'express'
+import authorize from '../middleware/auth.middleware.js';
+import { createSubscription, getUserSubscriptions } from '../controllers/subscription.controller.js';
 
 const subscriptionRouter = Router();
 
@@ -6,7 +8,7 @@ subscriptionRouter.get('/', (req,res) => res.send({ title : "GET all subscriptio
 
 subscriptionRouter.get('/:id', (req,res) => res.send({ title : "GET subscription"}))
 
-subscriptionRouter.post('/', (req,res) => res.send({ title : "CREATE subscription"}))
+subscriptionRouter.post('/', authorize, createSubscription)
 
 subscriptionRouter.put('/:id', (req,res) => res.send({ title : "UPDATE subscription"}))
 
@@ -15,7 +17,7 @@ subscriptionRouter.delete('/:id', (req,res) => res.send({ title : "DELETE subscr
 //upto this we have done our CURD operations, with get we've read,
 //with post we've create, with put we've update, with delete we've deleled the subscription
 
-subscriptionRouter.get('/user/:id', (req,res) => res.send({ title : "GET all user subscription"}))
+subscriptionRouter.get('/user/:id', authorize, getUserSubscriptions)
 
 subscriptionRouter.put('/:id/cancel', (req,res) => res.send({ title : "CANCEL subscription"}))
 
